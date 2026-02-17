1e9695769d8f6abbc7a29f72481959534518ca8b
Ivan <kolidov@yandex.ru>
2026-02-17 14:08:34 +0700

    updated scripts for generating tokens

--- backend-usability/utils/security.py
+++ backend-usability/utils/security.py
@@ -1,6 +1,11 @@
+from datetime import datetime, timedelta, timezone
+from typing import Dict, Optional
+
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
+from jose import jwt
 
+from utils.config import settings
 
 ph = PasswordHasher(
     time_cost=3,
@@ -11,7 +16,6 @@ ph = PasswordHasher(
 )
 
 def get_password_hash(password: str) -> str:
-    print(f"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!pass: {password}, {type(password)}")
     if len(password.encode("utf-8")) > 1024:
         raise ValueError("Password too long")
     
@@ -22,5 +26,49 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     try:
         ph.verify(hashed_password, plain_password)
         return True
+    
     except VerifyMismatchError:
         return False
+
+def create_access_token(data: Dict, expires_delta: Optional[timedelta] = None) -> str:
+
+    # Копируем данные, чтобы не изменять оригинальный словарь
+    to_encode = data.copy()
+    
+    # Устанавливаем время истечения токена
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(
+            minutes=settings.SECURITY_ACCESS_TOKEN
+        )
+    
+    # Добавляем время истечения в данные токена
+    to_encode.update({
+        "exp": expire,
+        "type": "access"
+    })
+    
+    return jwt.encode(
+            to_encode, 
+            key=settings.SECURITY_KEY, 
+            algorithm=settings.SECURITY_ALGORITHM,
+        )
+
+def create_refresh_token(data: Dict) -> str:
+    """"""
+
+    to_encode = data.copy()
+    
+    expire = datetime.now(timezone.utc) + timedelta(days=settings.SECURITY_REFRESH_TOKEN)
+    
+    to_encode.update({
+        "exp": expire,
+        "type": "refresh"
+    })
+    
+    return jwt.encode(
+        to_encode, 
+        key=settings.SECURITY_KEY, 
+        algorithm=settings.SECURITY_ALGORITHM,
+    )
